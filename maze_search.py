452d13c12caa6410da94f53e682de552b241a48e
Shreshtha731 <kumarshreshtha731@gmail.com>
2026-03-29 19:52:25 +0000

    feat: implemented neighbor detection and core search loop

--- maze_search.py
+++ maze_search.py
@@ -1,5 +1,6 @@
 import os
 import time
+from collections import deque
 
 MAZE = [
     "S #       ",
@@ -10,16 +11,24 @@ MAZE = [
     "   #     #"
 ]
 
-def print_maze(maze):
-    """Clears the terminal and draws the maze."""
+def print_maze(maze, visited=None):
     os.system('clear' if os.name == 'posix' else 'cls')
     print("\n--- AI Maze Search ---")
     for r in range(len(maze)):
         row_str = ""
         for c in range(len(maze[0])):
-            row_str += maze[r][c] + " "
+            if visited and (r, c) in visited:
+                row_str += ". "
+            else:
+                row_str += maze[r][c] + " "
         print(row_str)
     print("----------------------\n")
+    time.sleep(0.1)
 
-if __name__ == "__main__":
-    print_maze(MAZE)
+def get_neighbors(r, c, maze):
+    moves = []
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < len(maze) and 0 <= nc < len(maze[0]) and maze[nr][nc] != '#':
+            moves.append((nr, nc))
+    return moves
